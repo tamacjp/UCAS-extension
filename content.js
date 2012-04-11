2aa898b2bc223559c331c0cbb750f32e071fdd3a
tama <tama.eguchi@gmail.com>
2012-04-11 13:48:32 +0900

    content.js 整理

--- content.js
+++ content.js
@@ -1,59 +1,58 @@
 
-var conv = false;
-var count = 0;
+function procDocument(conv) {
+	var count = 0;
 
-// jQueryを使うまでもない。全ノード走査してテキストノードを書き換える。
-function procNode(node) {
-	console.log(node.nodeType);
-	switch (node.nodeType) {
-	case 1/*Node.ELEMENT_NODE*/:
-		procElementNode(node);
-		break;
+	// jQueryを使うまでもない。全ノード走査してテキストノードを書き換える。
+	function procNode(node) {
+		console.log(node.nodeType);
+		switch (node.nodeType) {
+		case 1/*Node.ELEMENT_NODE*/:
+			procElementNode(node);
+			break;
 
-	case 3/*Node.TEXT_NODE*/:
-		procTextNode(node);
-		break;
-	}
-}
-
-function procTextNode(node) {
-	node.textContent = node.textContent.replace(/[\u1401-\u167F]{2}/g, function(m) {
-		count += 1;
-		if (conv) {
-			var code = (m.charCodeAt(0) - 0x1400) + ((m.charCodeAt(1) - 0x1400) * 639);
-			return String.fromCharCode(code);
-		} else {
-			return m;
+		case 3/*Node.TEXT_NODE*/:
+			procTextNode(node);
+			break;
 		}
-	});
-}
-
-function procElementNode(node) {
-	for (var index = 0, len = node.childNodes.length; index < len; index++) {
-		procNode(node.childNodes[index]);
 	}
-}
 
-function procDocument(_conv) {
-	conv = _conv;
-	count = 0;
+	function procTextNode(node) {
+		node.textContent = node.textContent.replace(/[\u1401-\u167F]{2}/g, function(m) {
+			count += 1;
+			if (conv) {
+				var code = (m.charCodeAt(0) - 0x1400) + ((m.charCodeAt(1) - 0x1400) * 639);
+				return String.fromCharCode(code);
+			} else {
+			return m;
+			}
+		});
+	}
+
+	function procElementNode(node) {
+		for (var index = 0, len = node.childNodes.length; index < len; index++) {
+			procNode(node.childNodes[index]);
+		}
+	}
+
 	procNode(document.body);
+	return count;
 }
 
 // 事前処理で変換対象ウキャスを数える
-procDocument();
-if (count > 0) {
-	// pageAction有効化
-	chrome.extension.sendRequest({ 'cmd':'enable', 'count':count }, function(res) {});
-}
+(function() {
+	var count = procDocument();
+	if (count > 0) {
+		// 件数通知
+		chrome.extension.sendRequest({ 'cmd':'enable', 'count':count }, function(res) {});
+	}
+})();
 
 // contentのリクエストリスナ
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	var res = {};
 	if (request.cmd == 'convert') {
 		// convert指示
-		procDocument(true);
-		res.count = count;
+		res.count = procDocument(true);
 	}
 	sendResponse(res);
 });
