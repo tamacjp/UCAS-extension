40de28f789e2c4c576d94004fcc427af829feb11
tama <tama.eguchi@gmail.com>
2012-04-11 13:50:51 +0900

    pageAction → browserAction 変更

--- background.js
+++ background.js
@@ -3,22 +3,19 @@
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	var res = {};
 	if (request.cmd === 'enable') {
-		// pageAction有効化
-		var count = request.count;
-		chrome.pageAction.setTitle({ 'tabId':sender.tab.id, 'title':'has ' + count + ' letters' });
-		chrome.pageAction.show(sender.tab.id);
+		// 有効化
+		chrome.browserAction.setBadgeText({ 'tabId':sender.tab.id, 'text':'' + request.count });
 	}
 	sendResponse(res);
 });
 
-// pageActionクリック時処理
-chrome.pageAction.onClicked.addListener(function(tab) {
+// browserActionクリック時処理
+chrome.browserAction.onClicked.addListener(function(tab) {
 	// convertリクエスト
 	chrome.tabs.sendRequest(tab.id, { 'cmd':'convert' }, function(res) {
 		if (res.count) {
 			// 変換された
-			chrome.pageAction.setTitle({ 'tabId':tab.id, 'title':'decoded ' + res.count + ' letters' });
-			chrome.pageAction.show(sender.tab.id);
+			chrome.browserAction.setBadgeText({ 'tabId':tab.id, 'text':'' });
 		}
 	});
 });
